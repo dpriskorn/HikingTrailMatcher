589cdf4c6a86b8b3e0b017034f4bc3e949f1dd07
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-09-23 11:23:53 +0200

    pre-commit fixes

--- src/enrich_hiking_trails.py
+++ src/enrich_hiking_trails.py
@@ -2,43 +2,56 @@ import logging
 from enum import Enum
 from typing import List, Optional, Dict, Iterable
 
+import requests
 from pydantic import BaseModel, validate_arguments
-from wikibaseintegrator import WikibaseIntegrator
+from wikibaseintegrator import WikibaseIntegrator, wbi_config
 from wikibaseintegrator.datatypes import ExternalID
 from wikibaseintegrator.wbi_helpers import execute_sparql_query
 from rich.console import Console
+import questionary
 
 import config
+
 console = Console()
 logging.basicConfig(level=config.loglevel)
 logger = logging.getLogger(__name__)
 
+
 class Property(Enum):
     OSM_RELATION_ID = ""
 
+
+class WaymarkedResult(BaseModel):
+    id: int
+    name: str
+
+
 class EnrichHikingTrails(BaseModel):
     rdf_entity_prefix = "http://www.wikidata.org/entity/"
+    waymarked_results: List[WaymarkedResult] = []
 
     def __lookup_osm_relation_id__(self, label: str, aliases: List[str]) -> str:
-        # lookup in the waymarked trails database
+        self.__lookup_in_the_waymarked_trails_database__(search_term=label)
+        # TODO present a list for the user to choose from
 
+        result = questionary.select(
+            "What do you want to do?",
+            choices=["Order a pizza", "Make a reservation", "Ask for opening hours"],
+        ).ask()  # returns value of selection
+        console.print(result)
         # present the result to the user (we always want the first one)
         raise NotImplementedError()
 
-    def add_osm_property_to_items(self):
-        # get all hiking paths in sweden without osm id
-        items = self.__get_hiking_trails_missing_osm_id__()
-        for qid in items:
-            wbi = WikibaseIntegrator()
-            item = wbi.item.get(qid)
-            label = item.labels.get("sv")
-            aliases = item.aliases.get("sv")
-            osm_id = self.__lookup_osm_relation_id__(label=label, aliases=aliases)
-            if osm_id:
-                item.add_claims(claims=ExternalID(
-                    prop_nr=Property.OSM_RELATION_ID.value,
-                    value=osm_id
-                ))
+    def __lookup_in_the_waymarked_trails_database__(self, search_term) -> None:
+        url = (
+            f"https://hiking.waymarkedtrails.org/api/v1/list/search?query={search_term}"
+        )
+        result = requests.get(url=url)
+        if result.status_code == 200:
+            data = result.json()
+            console.print(data)
+            for result in data.get("results"):
+                self.waymarked_results.append(WaymarkedResult(**result))
 
     def __get_hiking_trails_missing_osm_id__(self) -> Iterable[str]:
         result = self.__get_sparql_result__()
@@ -48,15 +61,18 @@ class EnrichHikingTrails(BaseModel):
 
     @staticmethod
     def __get_sparql_result__():
+        wbi_config.config["USER_AGENT"] = "hiking-trail-scraper, see https://github.com/dpriskorn/hiking_trail_scraper/"
         # For now we limit to swedish trails
-        return execute_sparql_query("""
-        SELECT DISTINCT ?item ?itemLabel WHERE {
-          ?item wdt:P31 wd:Q2143825;
-                wdt:P17 wd:Q34.
-          minus{?item wdt:P402 []}
-          SERVICE wikibase:label { bd:serviceParam wikibase:language "sv,en". } # Hjälper dig hämta etiketten på ditt språk, om inte annat på engelska
-        }
-        """)
+        return execute_sparql_query(
+            """
+            SELECT DISTINCT ?item ?itemLabel WHERE {
+              ?item wdt:P31 wd:Q2143825;
+                    wdt:P17 wd:Q34.
+              minus{?item wdt:P402 []}
+              SERVICE wikibase:label { bd:serviceParam wikibase:language "sv,en". } # Hjälper dig hämta etiketten på ditt språk, om inte annat på engelska
+            }
+            """
+        )
 
     @validate_arguments
     def __extract_wcdqs_json_entity_id__(
@@ -76,3 +92,19 @@ class EnrichHikingTrails(BaseModel):
                     yield item_id
             if number_of_bindings := len(sparql_result["results"]["bindings"]):
                 logger.info(f"Yielded {yielded} bindings out of {number_of_bindings}")
+
+    def add_osm_property_to_items(self):
+        # get all hiking paths in sweden without osm id
+        items = self.__get_hiking_trails_missing_osm_id__()
+        for qid in items:
+            wbi = WikibaseIntegrator()
+            item = wbi.item.get(qid)
+            label = item.labels.get("sv")
+            aliases = item.aliases.get("sv")
+            osm_id = self.__lookup_osm_relation_id__(label=label, aliases=aliases)
+            if osm_id:
+                item.add_claims(
+                    claims=ExternalID(
+                        prop_nr=Property.OSM_RELATION_ID.value, value=osm_id
+                    )
+                )
