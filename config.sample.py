07ef9dd5ccfda9a52a58f63ab22d764631fcfcaf
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-09-23 17:33:27 +0200

    Improve handling of user agent

--- config.sample.py
+++ config.sample.py
@@ -4,3 +4,5 @@ loglevel = logging.INFO
 upload_to_wikidata = False
 user_name = ""
 bot_password = ""
+user_name_only = "input your user name here"
+user_agent = f"hiking_trail_matcher, see https://github.com/dpriskorn/hiking_trail_matcher/ User:{user_name_only}"
--- src/project_base_model.py
+++ src/project_base_model.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel
 from wikibaseintegrator import wbi_config  # type: ignore
 
+import config
+
 
 class ProjectBaseModel(BaseModel):
     @staticmethod
     def setup_wbi():
         wbi_config.config[
             "USER_AGENT"
-        ] = "hiking-trail-scraper, see https://github.com/dpriskorn/hiking_trail_scraper/"
+        ] = config.user_agent
