76787d5ffd940b2e693d7ac05d39ae2ca46f4f2e
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-09-29 10:51:32 +0200

    Support getting and parsing details from the Waymarked Trails API subroute.py: New file with class Subroute test_waymarked_result.py: test_fetch_details(), test_parse_details(), test_number_of_subroutes(): New tests waymarked_result.py: New attributes session, mapped_length, official_length, subroutes, details get_details(), __fetch_details__(), __parse_details__(), number_of_subroutes(), __names_of_subroutes__(), names_of_subroutes_as_string(): New methods

--- tests/test_waymarked_result.py
+++ tests/test_waymarked_result.py
@@ -32,3 +32,58 @@ class TestWaymarkedResult:
         assert wr.name == "Upplandsleden"
         assert wr.id == 241043
         assert len(wr.itinerary) == 0
+
+    # def test_get_details(self):
+    #     data = {
+    #         "type": "relation",
+    #         "id": 241043,
+    #         "name": "Upplandsleden",
+    #         "group": "NAT",
+    #         "symbol_description": "orange bar",
+    #         "symbol_id": "osmc_NAT_white_bar_orange",
+    #     }
+    #     wr = WaymarkedResult(**data)
+    #     wr.get_details()
+
+    def test_fetch_details(self):
+        data = {
+            "type": "relation",
+            "id": 241043,
+            "name": "Upplandsleden",
+            "group": "NAT",
+            "symbol_description": "orange bar",
+            "symbol_id": "osmc_NAT_white_bar_orange",
+        }
+        wr = WaymarkedResult(**data)
+        wr.__fetch_details__()
+        assert wr.details is not None
+
+    def test_parse_details(self):
+        data = {
+            "type": "relation",
+            "id": 241043,
+            "name": "Upplandsleden",
+            "group": "NAT",
+            "symbol_description": "orange bar",
+            "symbol_id": "osmc_NAT_white_bar_orange",
+        }
+        wr = WaymarkedResult(**data)
+        wr.__fetch_details__()
+        wr.__parse_details__()
+        assert wr.official_length == 500000.0
+        assert wr.mapped_length == 607380.0
+        assert wr.description == "hiking trail in Uppland"
+
+
+    def test_number_of_subroutes(self):
+        data = {
+            "type": "relation",
+            "id": 241043,
+            "name": "Upplandsleden",
+            "group": "NAT",
+            "symbol_description": "orange bar",
+            "symbol_id": "osmc_NAT_white_bar_orange",
+        }
+        wr = WaymarkedResult(**data)
+        wr.get_details()
+        assert wr.number_of_subroutes == 36
