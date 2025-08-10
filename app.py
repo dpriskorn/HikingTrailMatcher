111068aedb9344ca13ef4f424a3dce5920065823
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2025-08-10 02:50:46 +0200

    chore: tell user what we are doing

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ from src.models.enrich_hiking_trails import EnrichHikingTrails
 
 logging.basicConfig(level=config.loglevel)
 
+print(
+    f"Checking trails not updated for {config.max_days_between_new_check} "
+    f"days for lang:{config.language_code} and country:{config.country_qid}"
+)
 eht = EnrichHikingTrails()
 eht.add_osm_property_to_items()
