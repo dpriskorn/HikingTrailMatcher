5b07a595a363496c9ae2c4eeed53dbe6ac4a3c43
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2025-09-01 13:08:19 +0200

    fix: Set user agent

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import logging
 
+from wikibaseintegrator.wbi_config import config as wbconfig  # type:ignore
+
 import config
 from src.models.enrich_hiking_trails import EnrichHikingTrails
 
 logging.basicConfig(level=config.loglevel)
+wbconfig["USER_AGENT"] = config.user_agent
 
 print(
     f"Checking trails not updated for {config.max_days_between_new_check} "
